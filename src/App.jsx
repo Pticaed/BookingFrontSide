f471e08a0ce124b2e95cf95666ade1c4e87543de
Dmytro <dberezhkov2016@gmail.com>
2026-03-19 22:51:15 +0200

    searchbar done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Header } from "./widgets/header/ui/Header"; 
 import { Footer } from "./widgets/footer/ui/Footer";
 import Carousel from "./widgets/carousel/Carousel";
+import SearchBar from "./widgets/searchBar/SearchBar";
 import './App.css';
 
 export default function App() {
@@ -9,7 +10,12 @@ export default function App() {
     <div className="App">
       <Header />
       <main>
-        <Carousel></Carousel>
+         <div className="carouselWrapper">
+          <Carousel />
+          <div className="searchBarAbsolute">
+            <SearchBar />
+          </div>
+        </div>
       </main>
       <Footer />  
     </div>
---
+++ src/widgets/searchBar/SearchBar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./SearchBar.module.scss";
+import { Airplane, Calendar, Profile2User, ArrangeVertical, SearchNormal } from "iconsax-react";
+
+const SearchBar = () => {
+  return (
+    <div className={styles.searchBar}>
+      <div className={styles.column}>
+        <Airplane size="24" color="#333" variant="Linear"></Airplane>
+        <span>Where are you going?</span>
+      </div>
+
+      <div className={styles.column}>
+        <Calendar size="24" color="#333" variant="Linear"></Calendar>
+        <span>Check in - Check out</span>
+      </div>
+
+      <div className={styles.column}>
+        <Profile2User size="24" color="#333" variant="Linear"></Profile2User>
+        <span style={{marginRight: "30px"}}>2 adults 0 childrens 1 room</span>
+        <ArrangeVertical size="24" color="#333" variant="Linear"></ArrangeVertical>
+      </div>
+
+      <button className={styles.searchBtn}>
+        <SearchNormal size="24" color="white" variant="Linear"></SearchNormal>
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
